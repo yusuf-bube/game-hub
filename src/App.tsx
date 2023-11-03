d639cb0e658f5315f8878e0a0f87235f4d0a8d32
Yusuf Bube <119866160+yusuf-bube@users.noreply.github.com>
2023-11-03 10:39:25 +0100

    Install Chakra UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,12 @@
+import { Button } from "@chakra-ui/react";
+
 function App() {
-  return <h3>Starting</h3>;
+  return (
+    <>
+      <h3>Starting</h3>
+      <Button colorScheme="blue">Button</Button>
+    </>
+  );
 }
 
 export default App;
